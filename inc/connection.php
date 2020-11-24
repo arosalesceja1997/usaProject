c0eea79597d7b96ed6db7fbfde1eb460eadaadb7
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-11-23 18:23:53 -0600

    Anthony - php

--- inc/connection.php
+++ inc/connection.php
@@ -1,34 +1,11 @@
 <?php
-	// error_reporting(0);
-	// $usuario = "root";
-	// $contraseña = 1234;
-	// // var_dump($contraseña);
-	// // $db = new PDO('mysql:host=localhost;dbname=lufantasie', $usuario, $contraseña);
-	// // $db = mysqli_connect('localhost', $usuario, $contraseña);
-	// // mysqli_select_db('DATABASENAME', $db)
-	// // $db->query("SET NAMES 'utf8'");
-	// // $db->query("SET CHARACTER SET utf8");
-	// try {
-	// 	$mbd = new PDO('mysql:host=localhost;dbname=lufantasie', $usuario, $contraseña);
-	// 	// foreach($mbd->query('SELECT * from FOO') as $fila) {
-	// 	// 	print_r($fila);
-	// 	// }
-	// 	$mbd = null;
-	// } catch (PDOException $e) {
-	// 	print "¡Error!: " . $e->getMessage() . "<br/>";
-	// 	die();
-	// }
-	// error_reporting(0);
 	$usuario = "root";
 	$contraseña = 1234;
-	// var_dump($contraseña);
-	// $db = new PDO('mysql:host=localhost;dbname=lufantasie', $usuario, $contraseña);
+
 	try {
 		$db = new PDO('mysql:host=localhost;dbname=lufantasie', $usuario, $contraseña);
 		$db->query("SET NAMES 'utf8'");
 		$db->query("SET CHARACTER SET utf8");
-		// print "¡Error!: " . $db . "<br/>";
-		// $mbd = null;
 	} catch (PDOException $e) {
 		print "¡Error!: " . $e->getMessage() . "<br/>";
 		die();
